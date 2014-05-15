ac4a698f81e3bcdfdb9b0871684f9d817f31b7ab
力皓 <lihao.ylh@alibaba-inc.com>
2014-05-15 15:19:53 +0800

    change jquery id

--- src/jquery2/package.js
+++ src/jquery2/package.js
@@ -5,13 +5,13 @@
 if ( window.lofty ){
     jQuery.noConflict();
     
-    define( 'gallery/jquery/jqueryLatest', function(){
+    define( 'gallery/jquery/jquery-latest', function(){
         return jQuery;
     } );
     
     lofty.config({
         alias: {
-            'jquery': 'gallery/jquery/jqueryLatest'
+            'jquery': 'gallery/jquery/jquery-latest'
         }
     });
 }
